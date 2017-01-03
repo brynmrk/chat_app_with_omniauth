a9d9d8be179229e309a06d76b6a27ff6096ca4e3
Bryan Mark Fajutag <fbryanmark@gmail.com>
2017-01-03 23:33:47 +0000

    fix in secret keys

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,2 +1,2 @@
-GOOGLE_CONFIG = YAML.load_file("#{::Rails.root}/config/google.yml")[::Rails.env]
+# GOOGLE_CONFIG = YAML.load_file("#{::Rails.root}/config/google.yml")[::Rails.env]
 GOOGLE_SCOPES = 'contacts, plus.login, plus.me, userinfo.email, userinfo.profile, https://mail.google.com'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], { scope: FACEBOOK_SCOPES }
-  provider :google_oauth2, GOOGLE_CONFIG['client_id'], GOOGLE_CONFIG['client_secret'], { scope: GOOGLE_SCOPES }
-  provider :twitter, TWITTER_CONFIG['key'], TWITTER_CONFIG['secret'], { :secure_image_url => 'true', :image_size => 'original' }
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], { scope: FACEBOOK_CONFIG }
+  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], { scope: GOOGLE_CONFIG }
+  provider :twitter, ENV['twitter_key'], ENV['twitter_secret'], { :secure_image_url => 'true', :image_size => 'original' }
   configure do |config|
       config.failure_raise_out_environments = []
   end
