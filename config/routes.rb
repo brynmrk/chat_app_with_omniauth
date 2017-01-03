7c8ee4f706803ff46b8f8917bea1fbae8acef493
Bryan Mark Fajutag <fbryanmark@gmail.com>
2017-01-03 07:41:31 +0000

    fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   resources :chat_rooms, only: [:new, :create, :show, :index]
-  mount ActionCable.server => '/cable'
   
   root to: 'home#index'
 end
