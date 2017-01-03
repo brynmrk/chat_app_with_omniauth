7f83c1aff7c1c054083ec1c888b2c12a96e64cce
Bryan Mark Fajutag <fbryanmark@gmail.com>
2017-01-03 23:35:32 +0000

    fix omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], { scope: FACEBOOK_CONFIG }
-  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], { scope: GOOGLE_CONFIG }
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], { scope: 'manage_pages, pages_show_list, publish_pages' }
+  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], { scope: 'contacts, plus.login, plus.me, userinfo.email, userinfo.profile, https://mail.google.com' }
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret'], { :secure_image_url => 'true', :image_size => 'original' }
   configure do |config|
       config.failure_raise_out_environments = []
