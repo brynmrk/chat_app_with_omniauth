e2036c4d00b18ad52d27526ab63b0e9d302a69bd
Bryan Mark Fajutag <fbryanmark@gmail.com>
2017-01-04 00:45:04 +0000

    fix need login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
+  before_action :authenticate_user!
   
   protected
     def layout_by_resource
